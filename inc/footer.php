3429d7c97105dac0153eabcc15eca345c8e9e4f9
Stella Azevedo <costadeazevedostella@gmail.com>
2025-10-10 15:09:52 -0300

    Finalizando o footer responsivo
    Parei em: media (min-width: 577px) and (max-width: 768px)

--- inc/footer.php
+++ inc/footer.php
@@ -8,7 +8,7 @@
        <div class="col-xl-6 col-lg-5 col-md-5 text-center">
             <div class="col-md-12 d-flex justify-content-center format">
                 <h1 class="san txtfooter me-xl-2 me-md-2">Xerife Pastell</h1>
-                <img src="<?php echo BASEURL; ?>assets/img/icon.png" alt="Logo" width="50" height="50" class="d-inline-block mb-2">
+                <img src="<?php echo BASEURL; ?>assets/img/icon.png" alt="Logo" class="d-inline-block mb-2 icon-footer">
             </div>
             <div class="d-flex justify-content-center">
                 <h3 class="subfooter">O melhor pastel da região!</h3>
@@ -22,7 +22,6 @@
             </div>
        </div>
        <div class="col-xl-3 col-lg-3 col-md-3 d-flex flex-column p-0 bg-footer2" >
-            <img src="<?php echo BASEURL; ?>assets/img/footer2.png" alt="Logo">
        </div>
    </div>
 </footer>

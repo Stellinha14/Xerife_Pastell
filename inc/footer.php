c2f4ee7412d4e8d5f1056ec68bad93a347550357
Stella Azevedo <costadeazevedostella@gmail.com>
2025-10-10 11:29:20 -0300

    responsivo footer

--- inc/footer.php
+++ inc/footer.php
@@ -1,11 +1,11 @@
 <footer class="container-fluid">
    <div class="row p-0">
-       <div class="col-2 p-0 bg-footer">
+       <div class="col-xl-2 p-0 bg-footer">
        </div>
-       <div class="col-2 d-flex flex-column">
+       <div class="col-xl-3 d-flex flex-column">
             <img src="<?php echo BASEURL; ?>assets/img/persona.png" class="xerifinho mt-auto mx-auto">
        </div>
-       <div class="col-4 col-lg-6">
+       <div class="col-xl-6 col-lg-6">
             <div class="d-flex justify-content-center format">
                 <h1 class="san txtfooter me-2">Xerife Pastell</h1>
                 <img src="<?php echo BASEURL; ?>assets/img/icon.png" alt="Logo" width="50" height="50" class="d-inline-block mb-2">
@@ -21,7 +21,7 @@
                 <h5> &copy;2023 Xerife Pastell </h5>
             </div>
        </div>
-       <div class="col-4 col-lg-3 d-flex flex-column p-0 bg-footer2" >
+       <div class="col-xl-2 col-lg-2 d-flex flex-column p-0 bg-footer2" >
        </div>
    </div>
 </footer>

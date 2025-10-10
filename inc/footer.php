116838b74ba29c1f6a335e9989f9bf2f25597a7e
Stella Azevedo <costadeazevedostella@gmail.com>
2025-10-10 12:28:00 -0300

    footer responsivo
    ta bonito ate a medida 862px

--- inc/footer.php
+++ inc/footer.php
@@ -1,13 +1,13 @@
 <footer class="container-fluid">
    <div class="row p-0">
-       <div class="col-xl-2 p-0 bg-footer">
+       <div class="col-xl-2 col-lg-2 col-md-2 p-0 bg-footer">
        </div>
-       <div class="col-xl-3 d-flex flex-column">
-            <img src="<?php echo BASEURL; ?>assets/img/persona.png" class="xerifinho mt-auto mx-auto">
+       <div class="col-xl-1 col-lg-2 col-md-2 d-flex flex-column">
+            <img src="<?php echo BASEURL; ?>assets/img/persona.png" class="xerifinho_footer mt-auto mx-auto">
        </div>
-       <div class="col-xl-6 col-lg-6">
-            <div class="d-flex justify-content-center format">
-                <h1 class="san txtfooter me-2">Xerife Pastell</h1>
+       <div class="col-xl-6 col-lg-5 col-md-5">
+            <div class="col-md-12 d-flex justify-content-center format">
+                <h1 class="san txtfooter me-xl-2 me-md-2">Xerife Pastell</h1>
                 <img src="<?php echo BASEURL; ?>assets/img/icon.png" alt="Logo" width="50" height="50" class="d-inline-block mb-2">
             </div>
             <div class="d-flex justify-content-center">
@@ -17,11 +17,11 @@
                 <img src="<?php echo BASEURL; ?>assets/img/iconfooter.png" class="imgfooter">
             </div>
             <div class="text-center txtf">
-                <h4>pastellxerife@gmail.com.br</h4>
-                <h5> &copy;2023 Xerife Pastell </h5>
+                <p>pastellxerife@gmail.com.br</p>
+                <p> &copy;2023 Xerife Pastell </p>
             </div>
        </div>
-       <div class="col-xl-2 col-lg-2 d-flex flex-column p-0 bg-footer2" >
+       <div class="col-xl-3 col-lg-3 col-md-3 d-flex flex-column p-0 bg-footer2" >
        </div>
    </div>
 </footer>

1c75e15b60dad96385039fa0a1df205ed800f574
Stella Azevedo <costadeazevedostella@gmail.com>
2025-11-05 00:43:11 -0300

    Design do teste 2 e organização de teste.js
    Design da questão 2 praticamente finalizado. Falta acrescentar uma pequena logica para deixar mais dinâmico.

--- assets/js/custom/teste.js
+++ assets/js/custom/teste.js
@@ -1,151 +1,180 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const box = document.querySelectorAll(".box");
-    console.log(box);
-    let drop = document.querySelectorAll(".drop");
-    const slots = document.querySelectorAll(".slot");
-    let btn = document.getElementById("btnSig");
-    let pai;
-    let cont = 0;
-    let guardados = [];
-    let apareceu = false
-    let significado = document.querySelector(".significado");
 
-    //para aparecer o botão significado 
-    btn.addEventListener("click", function () {
-        //event.preventDefault();
-        //console.log("Entrei")
-        significado.classList.toggle("show");
+    // ==========================
+    // QUESTÃO 1 - CLASSIFICAÇÃO DOS PASTEIS
+    // ===========================
 
-        if (significado.classList.contains('show')) {
-            btn.innerHTML = "Esconder Significado"
-        } else {
-            btn.innerHTML = "Ver Significado";
-        }
-    });
+    function questao_1() {
+        const box = document.querySelectorAll(".box");
+        console.log(box);
+        let drop = document.querySelectorAll(".drop");
+        const slots = document.querySelectorAll(".slot");
+        let btn = document.getElementById("btnSig");
+        let pai;
+        let cont = 0;
+        let guardados = [];
+        let apareceu = false
+        let significado = document.querySelector(".significado");
 
-    //para aparecer o botão significado !quando todos os slots estiverem no drop! 
-    function atualizarCont() {
-        drop.forEach(d => {
-            const filhoDrop = d.querySelectorAll(".box")
-            filhoDrop.forEach(filho => {
-                if (!guardados.includes(filho)) {
-                    guardados.push(filho);
+        //para aparecer o botão significado 
+        btn.addEventListener("click", function () {
+            //event.preventDefault();
+            //console.log("Entrei")
+            significado.classList.toggle("show");
+
+            if (significado.classList.contains('show')) {
+                btn.innerHTML = "Esconder Significado"
+            } else {
+                btn.innerHTML = "Ver Significado";
+            }
+        });
+
+        //para aparecer o botão significado !quando todos os slots estiverem no drop! 
+        function atualizarCont() {
+            drop.forEach(d => {
+                const filhoDrop = d.querySelectorAll(".box")
+                filhoDrop.forEach(filho => {
+                    if (!guardados.includes(filho)) {
+                        guardados.push(filho);
+                    }
+                });
+                if (guardados.length >= 5) {
+                    btn.style.display = "block";
+                    apareceu = true;
+                    guardados = [];
                 }
             });
-            if (guardados.length >= 5) {
-                btn.style.display = "block";
-                apareceu = true;
-                guardados = [];
-            }
-        });
-    }
-
-
-    box.forEach(box_unic => {
-        const slotPai = box_unic.parentElement;
-
-        if (slotPai) {
-            box_unic.setAttribute("data-origem", slotPai.dataset.slot);
         }
-        box_unic.addEventListener('dragstart', e => {
-            e.dataTransfer.setData('text/plain', e.currentTarget.id);
-            let pai_element = document.getElementById(e.currentTarget.id).parentElement;
-            if (pai_element.classList.contains('drop')) {
-                pai = pai_element;
+
+
+        box.forEach(box_unic => {
+            const slotPai = box_unic.parentElement;
+
+            if (slotPai) {
+                box_unic.setAttribute("data-origem", slotPai.dataset.slot);
             }
-        });
-    });
-
-    drop.forEach(drop_unic => {
-        drop_unic.addEventListener('dragover', e => {
-            e.preventDefault();
-            drop_unic.classList.add('over')
-        });
-
-        drop_unic.addEventListener('dragleave', () => {
-            drop_unic.classList.remove('over')
+            box_unic.addEventListener('dragstart', e => {
+                e.dataTransfer.setData('text/plain', e.currentTarget.id);
+                let pai_element = document.getElementById(e.currentTarget.id).parentElement;
+                if (pai_element.classList.contains('drop')) {
+                    pai = pai_element;
+                }
+            });
         });
 
-        drop_unic.addEventListener('drop', e => {
+        drop.forEach(drop_unic => {
+            drop_unic.addEventListener('dragover', e => {
+                e.preventDefault();
+                drop_unic.classList.add('over')
+            });
 
-            const id = e.dataTransfer.getData('text/plain');
-            const element = document.getElementById(id);
+            drop_unic.addEventListener('dragleave', () => {
+                drop_unic.classList.remove('over')
+            });
 
-            //verifica se já existe um box dentro da drop e faz a troca
-            if (drop_unic.querySelector('.box')) {
-                const opc_antiga = drop_unic.querySelector('.box');
-                let origem = element.parentElement;
-                origem.appendChild(opc_antiga);
+            drop_unic.addEventListener('drop', e => {
 
+                const id = e.dataTransfer.getData('text/plain');
+                const element = document.getElementById(id);
+
+                //verifica se já existe um box dentro da drop e faz a troca
+                if (drop_unic.querySelector('.box')) {
+                    const opc_antiga = drop_unic.querySelector('.box');
+                    let origem = element.parentElement;
+                    origem.appendChild(opc_antiga);
+
+                    drop_unic.appendChild(element);
+                    drop_unic.classList.remove('over');
+                    element.classList.add('placed');
+                    return;
+                }
+                //mostrar nr
+                if (pai) {
+                    const span_sumido = pai.querySelector('span')
+                    span_sumido.style.display = 'block';
+                }
+
+                //mostra drop_sab
+                const slot = element.parentElement;
+                const drop_sab = slot.querySelector('.drop-sab');
+                if (drop_sab) {
+                    drop_sab.style.display = 'flex';
+                }
+
+                //move a box para dentro do drop
                 drop_unic.appendChild(element);
                 drop_unic.classList.remove('over');
                 element.classList.add('placed');
-                return;
-            }
-            //mostrar nr
-            if (pai) {
-                const span_sumido = pai.querySelector('span')
-                span_sumido.style.display = 'block';
-            }
 
-            //mostra drop_sab
-            const slot = element.parentElement;
-            const drop_sab = slot.querySelector('.drop-sab');
-            if (drop_sab) {
-                drop_sab.style.display = 'flex';
-            }
-
-            //move a box para dentro do drop
-            drop_unic.appendChild(element);
-            drop_unic.classList.remove('over');
-            element.classList.add('placed');
-
-            // esconde o span dentro do drop
-            const span = drop_unic.querySelector('span')
-            if (span) {
-                span.style.display = "none";
-            }
-            atualizarCont();
-        });
-    });
-
-    const btnLimpa = document.getElementById('btnLimpa');
-
-    if (btnLimpa) {
-        btnLimpa.addEventListener('click', () => {
-
-
-            box.forEach(b => {
-
-                const pai_box = b.parentElement;
-
-                if (pai_box.closest('.cont_drop')) {
-                    cont + 1;
-                    if (cont === 0 && apareceu) {
-                        btn.style.display = "none";
-                        significado.classList.remove("show");  
-                        btn.innerHTML="Ver Significado"     
-                    }
-                }
-
-                //colocando o span do drop novamente
-                const span = pai_box.querySelector('span');
+                // esconde o span dentro do drop
+                const span = drop_unic.querySelector('span')
                 if (span) {
-                    span.style.display = "block";
+                    span.style.display = "none";
                 }
-
-                //colocando a box no slot correspondente
-                const origemId = b.getAttribute("data-origem");
-                const origem = document.querySelector(`.slot[data-slot="${origemId}"]`);
-                const drop_sab = origem.querySelector('.drop-sab');
-
-                if (origem) {
-                    drop_sab.style.display = "none";
-                    origem.appendChild(b);
-                    b.classList.remove('placed');
-                }
+                atualizarCont();
             });
         });
+
+        const btnLimpa = document.getElementById('btnLimpa');
+
+        if (btnLimpa) {
+            btnLimpa.addEventListener('click', () => {
+
+
+                box.forEach(b => {
+
+                    const pai_box = b.parentElement;
+
+                    if (pai_box.closest('.cont_drop')) {
+                        cont + 1;
+                        if (cont === 0 && apareceu) {
+                            btn.style.display = "none";
+                            significado.classList.remove("show");
+                            btn.innerHTML = "Ver Significado"
+                        }
+                    }
+
+                    //colocando o span do drop novamente
+                    const span = pai_box.querySelector('span');
+                    if (span) {
+                        span.style.display = "block";
+                    }
+
+                    //colocando a box no slot correspondente
+                    const origemId = b.getAttribute("data-origem");
+                    const origem = document.querySelector(`.slot[data-slot="${origemId}"]`);
+                    const drop_sab = origem.querySelector('.drop-sab');
+
+                    if (origem) {
+                        drop_sab.style.display = "none";
+                        origem.appendChild(b);
+                        b.classList.remove('placed');
+                    }
+                });
+            });
+        }
     }
+
+    // ==========================
+    // QUESTÃO 2 - ADJETIVOS NOS PASTEIS
+    // ===========================
+    function questao_2() {
+
+    }
+
+    // ==========================
+    // QUESTÃO 3 - RELACIONAR PASTEIS
+    // ===========================
+    function questao_3() {
+
+    }
+
+    // ==========================
+    // CHAMADA DAS FUNÇÕES
+    // ===========================
+
+    questao_1();
+    questao_2();
+    questao_3();
 
 });
--- views/teste.php
+++ views/teste.php
@@ -60,7 +60,7 @@ include HEADER_TEMPLATE;
 
             <p class="mt-2">Coloque esses pastéis na ordem que mais te agrada, do primeiro ao último. <span class="font2">NÃO PENSE DEMAIS.</span> Siga o seu instinto de xerife e escolha pela emoção do momento!</p>
             <p>Depois é so clicar em <span class="font2">"Ver Significado"</span> e descobrir o que suas escolhas revelam sobre você!</p>
-            
+
         </div>
 
         <div class="container-geral" arial-label="Teste Tibetano 1">
@@ -174,140 +174,43 @@ include HEADER_TEMPLATE;
     <section arial-label="Teste Tibetano 2">
         <!-- enunciado -->
         <div class="enum txtadivinha tit">
-            <h1><span class="font2">Questão 02.</span> O que!</h1>
-            <p>Imagine que você acaba de chegar na <span class="font2">Xerife Pastell</span>, onde o cheiro é simplesmente irresistível! Na sua frente estão <span class="font2">5 pastéis lendários:</span>
+            <h1><span class="font2">Questão 02.</span> O Xerife das Palavras!</h1>
+            <p>Na sua pastelaria favorita, cada sabor tem uma personalidade própria. Agora é hora de dar <span class="font2">um adjetivo ou característica</span> a cada item abaixo. Use <span class="font2">só o primeiro que vier à sua cabeça</span> e não pense demais!
             </p>
-            <!-- sabor + icon -->
-            <div class="d-flex align-items-center mb-2">
-                <img src="<?php echo BASEURL; ?>assets/img/i_queijo.png" class="icons" alt="icon de queijo">
-                <p class="font2 m-0">Queijo;</p>
-            </div>
-            <div class="d-flex align-items-center mb-2">
-                <img src="<?php echo BASEURL; ?>assets/img/i_bauru.png" class="icons" alt="icon de bauru">
-                <p class="font2 m-0">Bauru;</p>
-            </div>
-            <div class="d-flex align-items-center mb-2">
-                <img src="<?php echo BASEURL; ?>assets/img/i_camarao.png" class="icons" alt="icon de camarão">
-                <p class="font2 m-0">Camarão;</p>
-            </div>
-            <div class="d-flex align-items-center mb-2">
-                <img src="<?php echo BASEURL; ?>assets/img/i_morango.png" class="icons" alt="icon de morango">
-                <p class="font2 m-0">Morango;</p>
-            </div>
-            <div class="d-flex align-items-center">
-                <img src="<?php echo BASEURL; ?>assets/img/i_beijinho.png" class="icons" alt="icon de beijinho">
-                <p class="font2 m-0">Beijinho.</p>
-            </div>
-
-            <p class="mt-2">Coloque esses pastéis na ordem que mais te agrada, do primeiro ao último. <span class="font2">NÃO PENSE DEMAIS.</span> Siga o seu instinto de xerife e escolha pela emoção do momento!</p>
-            <p>Depois é so clicar em <span class="font2">"Ver Significado"</span> e descobrir o que suas escolhas revelam sobre você!</p>
-            
         </div>
 
         <div class="container-geral" arial-label="Teste Tibetano 1">
-            <!-- Box -->
-            <div class="cont_test1">
-                <div class="column-1">
-                    <h1 class="san text-center tit-column">Sabores</h1>
-                    <div class="slot" data-slot="1">
-                        <div id="box1" class="box" draggable="true">
-                            <img src="<?php echo BASEURL; ?>assets/img/queijo.png" class="img-fluid pasteis-sabores">
-                        </div>
-                        <div class="san drop-sab">
-                            <span>Pastel de Queijo</span>
-                        </div>
-                    </div>
-                    <div class="slot" data-slot="2">
-                        <div id="box2" class="box" draggable="true">
-                            <img src="<?php echo BASEURL; ?>assets/img/camarao.png" class="img-fluid pasteis-sabores">
-                        </div>
-                        <div class="san drop-sab">
-                            <span>Pastel de Camarão</span>
-                        </div>
-                    </div>
-                    <div class="slot" data-slot="3">
-                        <div id="box3" class="box" draggable="true">
-                            <img src="<?php echo BASEURL; ?>assets/img/bauru.png" class="img-fluid pasteis-sabores">
-                        </div>
-                        <div class="san drop-sab">
-                            <span>Pastel de Bauru</span>
-                        </div>
-                    </div>
-                    <div class="slot" data-slot="4">
-                        <div id="box4" class="box" draggable="true">
-                            <img src="<?php echo BASEURL; ?>assets/img/morango.png" class="img-fluid pasteis-sabores">
-                        </div>
-                        <div class="san drop-sab">
-                            <span>Pastel de Morango</span>
-                        </div>
-                    </div>
-                    <div class="slot" data-slot="5">
-                        <div id="box5" class="box" draggable="true">
-                            <img src="<?php echo BASEURL; ?>assets/img/beijinho.png" class="img-fluid pasteis-sabores">
-                        </div>
-                        <div class="san drop-sab">
-                            <span>Pastel de beijinho</span>
-                        </div>
-                    </div>
-                </div>
-                <!-- Drop -->
-                <div class="column-2 opc">
-                    <h1 class="san text-center tit-column">Posições</h1>
-                    <div class="cont_drop">
-                        <span>1°</span>
-                        <div id="drop1" class="drop"><span>1°</span></div>
-                    </div>
-                    <div class="cont_drop">
-                        <span>2°</span>
-                        <div id="drop2" class="drop"><span>2°</span></div>
-                    </div>
-                    <div class="cont_drop">
-                        <span>3°</span>
-                        <div id="drop3" class="drop"><span>3°</span></div>
-                    </div>
-                    <div class="cont_drop">
-                        <span>4°</span>
-                        <div id="drop4" class="drop"><span>4°</span></div>
-                    </div>
-                    <div class="cont_drop">
-                        <span>5°</span>
-                        <div id="drop5" class="drop"><span>5°</span></div>
-                    </div>
-                </div>
+            <!-- input + paragrafo -->
+            <div class="font2 adj mb-4">
+                <p>O Pastel de Vento é ...</p>
+                <input type="text">
             </div>
-            <!-- button -->
-            <div class="carta-btn justify-content-center align-items-center">
-                <button type="button" id="btnSig" class="btn-en tit btnSig">Ver Significado</button>
-                <button type="button" id="btnLimpa" class="btn-lim tit">Resetar</button>
+
+            <div class="font2 adj mb-4">
+                <p>Pastel Doce é ...</p>
+                <input type="text">
             </div>
-            <!-- signicados -->
-            <div class="d-flex flex-column significado">
-                <p class="text-center txtadivinha tit font1">
-                    A maneira como você ordenou os pasteis, representa a prioridade que cada um destes campos abaixo tem em sua vida:
-                </p>
-                <ul class="lista-signi">
-                    <li class="text-start txtadivinha tit font1">
-                        <span class="font2">Pastel de Queijo:</span>
-                        Estabilidade e Segurança
-                    </li>
-                    <li class="text-start txtadivinha tit font1">
-                        <span class="font2">Pastel de Bauru:</span>
-                        Amizade e Alegria
-                    </li>
-                    <li class="text-start txtadivinha tit font1">
-                        <span class="font2">Pastel de Camarão:</span>
-                        Paixão e Intensidade
-                    </li>
-                    <li class="text-start txtadivinha tit font1">
-                        <span class="font2">Pastel de Morango:</span>
-                        Afeto e Doçura
-                    </li>
-                    <li class="text-start txtadivinha tit font1">
-                        <span class="font2">Pastel de Beijinho:</span>
-                        Nostalgia e Inocência
-                    </li>
-                </ul>
+
+            <div class="font2 adj mb-4">
+                <p>Refrigerante gelado é ...</p>
+                <input type="text">
             </div>
+
+            <div class="font2 adj mb-4">
+                <p>Pastel de Queijo Derretido é ...</p>
+                <input type="text">
+            </div>
+
+            <div class="font2 adj mb-4">
+                <p>Pastel Crocante é ...</p>
+                <input type="text">
+            </div>
+            <!-- obs. -->
+            <p class="text-center"><em class="font4 tit"><span class="font2">Lembre-se:</span> não existe resposta certa ou errada, o que vale é o que você sente sobre cada sabor. Siga seu instinto de xerife e divirta-se com a sua própria escolha!</em></p>
+
+            <!-- button space  -->
+            <button type="button" id="btnSig2" class="btn-en tit btnSig2">Ver Significado</button>
+            <button type="button" id="btnLimpa2" class="btn-lim tit">Resetar</button>
         </div>
     </section>
 </div>
